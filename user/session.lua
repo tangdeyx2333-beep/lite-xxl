7a51e9476b1f3456664d6932dd3efa82489e97d3
tangjinyuan <tangdeyx2333@gmail.com>
2026-06-20 12:23:07 +0800

    feat：优化打开多窗口逻辑

--- user/session.lua
+++ user/session.lua
@@ -1,1 +1,1 @@
-return {recents={[1]="F:\\.claude"}, window={[1]=2048,[2]=1152,[3]=255,[4]=113,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
+return {recents={[1]="F:\\.pnpm-store",[2]="F:\\aboutModels"}, window={[1]=1805,[2]=922,[3]=436,[4]=83,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
